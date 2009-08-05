f2c2132973157339d017228eec994d0f9a6b786e
Thibaud Guillaume-Gentil <guillaumegentil@gmail.com>
2009-08-05 16:27:37 +0200

    Updated to work directly in test mode

--- lib/action_mailer_cache_delivery.rb
+++ lib/action_mailer_cache_delivery.rb
@@ -1,19 +1,19 @@
 ActionMailer::Base.class_eval do
-
-  DELIVERIES_CACHE_PATH =
-    File.join(RAILS_ROOT,'tmp','cache','action_mailer_cache_deliveries.cache')
-
-  def perform_delivery_cache(mail)
-    deliveries << mail
-    File.open(DELIVERIES_CACHE_PATH,'w') do |f|
-      Marshal.dump(deliveries, f)
-    end
+  
+  DELIVERIES_CACHE_PATH = File.join(RAILS_ROOT,'tmp','cache','action_mailer_cache_deliveries.cache')
+  
+  def perform_delivery_test(mail)
+    mails = File.exist?(DELIVERIES_CACHE_PATH) ? File.open(DELIVERIES_CACHE_PATH,'r') { |f| Marshal.load(f) } : []
+    mails << mail
+    File.open(DELIVERIES_CACHE_PATH,'w') { |f| Marshal.dump(mails, f) }
   end
-
-  def self.cached_deliveries
-    File.open(DELIVERIES_CACHE_PATH,'r') do |f|
-      Marshal.load(f)
-    end
+  
+  def self.deliveries
+    File.exist?(DELIVERIES_CACHE_PATH) ? File.open(DELIVERIES_CACHE_PATH,'r') { |f| Marshal.load(f) } : []
+  end 
+  
+  def self.clear_deliveries
+    File.open(DELIVERIES_CACHE_PATH,'w') { |f| Marshal.dump([], f) }
   end
-
+  
 end
--- tasks/action_mailer_cache_delivery_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :action_mailer_cache_delivery do
-#   # Task goes here
-# end
