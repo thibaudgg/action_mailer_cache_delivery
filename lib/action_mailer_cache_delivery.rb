7497146b59f0d7cedb02abd106c4d666140acc77
Thibaud Guillaume-Gentil <guillaumegentil@gmail.com>
2009-08-07 16:39:58 +0200

    Added tmp/cache folder creation if not exists

--- lib/action_mailer_cache_delivery.rb
+++ lib/action_mailer_cache_delivery.rb
@@ -1,10 +1,14 @@
 ActionMailer::Base.class_eval do
   
+  TEMP_DIR_PATH = File.join(RAILS_ROOT,'tmp')
+  CACHE_DIR_PATH = File.join(RAILS_ROOT,'tmp','cache')
   DELIVERIES_CACHE_PATH = File.join(RAILS_ROOT,'tmp','cache','action_mailer_cache_deliveries.cache')
   
   def perform_delivery_test(mail)
     mails = File.exist?(DELIVERIES_CACHE_PATH) ? File.open(DELIVERIES_CACHE_PATH,'r') { |f| Marshal.load(f) } : []
     mails << mail
+    Dir.mkdir(TEMP_DIR_PATH) unless File.exist?(TEMP_DIR_PATH)
+    Dir.mkdir(CACHE_DIR_PATH) unless File.exist?(CACHE_DIR_PATH)
     File.open(DELIVERIES_CACHE_PATH,'w') { |f| Marshal.dump(mails, f) }
   end
   
